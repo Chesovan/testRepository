1b0f74903656bf560d9019efc6adc37a23119180
MariusChesovan <marius.chesovan@evozon.com>
2016-07-13 15:13:49 +0300

    asdasdas

--- src/test/java/com/pages/InboxPage.java
+++ src/test/java/com/pages/InboxPage.java
@@ -4,16 +4,16 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 
-public class VacationRequestPage extends PageObject{
+public class InboxPage extends PageObject{
 	
 	@FindBy(css = ".main-menu li:nth-child(9) a")
 	private WebElementFacade vacationLi;
 	
-	@FindBy(css = ".nav-header:first-child+li a")
-	private WebElementFacade newVacationRequest;
-	
-	@FindBy(css = "input[name='startDate']") 
-	private WebElementFacade startDate;
+	@FindBy(css = ".aui-column-content .carousel-slider:nth-child(3) ul li:nth-child(3) a")
+	private WebElementFacade inboxAccess;
+		
+	@FindBy(css = "tbody tr:nth-child(3) td:first-child input") 
+	private WebElementFacade checkUncheckBox;
 	
 	@FindBy(css = "input[name='endDate']") 
 	private WebElementFacade endDate;
